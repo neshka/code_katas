b3db6b18b784ab90fe20856470e8f8ac828cdc28
neshka <neshka_mind@yahoo.com>
2017-05-08 16:50:19 +0100

    add new katana with switch statement

---
+++ give_the_note.php
@@ -0,0 +1,46 @@
+<?php
+
+//0&ndash;39 p &ndash; F
+//40&ndash;54 p &ndash; E
+//55&ndash;69 p &ndash; D
+//70&ndash;84 p &ndash; C
+//85&ndash;98 p &ndash; B
+//99&ndash;100 p &ndash; A
+
+//switch (true) {
+//
+//    case ($totaltime <= 1):
+//        echo "That was fast!";
+//        break;
+//
+//    case ($totaltime <= 5):
+//        echo "Not fast!";
+//        break;
+//
+//    case ($totaltime <= 10):
+//        echo "That's slooooow";
+//        break;
+//}
+
+//if ($i == 0) {
+//    echo "i equals 0";
+//} elseif ($i == 1) {
+//    echo "i equals 1";
+//} elseif ($i == 2) {
+//    echo "i equals 2";
+//}
+//
+//switch ($i) {
+//    case 0:
+//        echo "i equals 0";
+//        break;
+//    case 1:
+//        echo "i equals 1";
+//        break;
+//    case 2:
+//        echo "i equals 2";
+//        break;
+//}
+
+function which_note
+
