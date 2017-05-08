3f1755e226e60fcebad7e43779017f0a5b7ae685
neshka <neshka_mind@yahoo.com>
2017-05-08 16:31:21 +0100

    add biggest natural

---
+++ the_biggest_natural_no.php
@@ -0,0 +1,17 @@
+<?php
+
+function biggest_natural($a, $b, $c){
+    if ($a>$b && $a>$c){
+        return $a;
+    }elseif ($b>$c){
+        return $b;
+    }else{
+        return $c;
+    }
+}
+
+echo biggest_natural(1,2,3);
+echo "<br>";
+echo biggest_natural(5,4,3);
+echo "<br>";
+echo biggest_natural(5,7,6);
