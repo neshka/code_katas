e4d6fc25ba27f0188342c19713ff7c0f97489dd4
neshka <neshka_mind@yahoo.com>
2017-05-09 22:28:13 +0100

    add sqare

---
+++ stars/star_square.php
@@ -0,0 +1,16 @@
+<?php
+
+function drwaSquare($long){
+    for ($i = 0; $i <= $long; $i++) {
+        for ($j = 0; $j <= $long; $j++) {
+            echo '*';
+        }
+
+        echo '<br>';
+    }
+
+}
+
+drwaSquare(5);
+
+
